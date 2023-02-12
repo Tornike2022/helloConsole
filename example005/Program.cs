acdf2c33f86a75ed1b34e58323f718c545a9ed64
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-12 12:44:37 +0300

    addedifelse

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input the username ");
+string username = Console.ReadLine();
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Hooray, This is Maria!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
